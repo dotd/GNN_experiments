de11835999da4090e724a8c3df11d913f5cdf4fd
Dotan Di Castro <dotan.dicastro@gmail.com>
2020-12-21 22:13:30 +0200

    Added torch geometric

--- src/utils/proxy_utils.py
+++ src/utils/proxy_utils.py
@@ -1,7 +1,11 @@
 from definitions_gnn_experiments import ROOT_DIR
+from src.utils.file_utils import file_exists
 
 
 def load_user_password():
+    file = f"{ROOT_DIR}/login_credentials.txt"
+    if not file_exists(file):
+        return False
     with open(f"{ROOT_DIR}/login_credentials.txt", 'r') as file:
         data = file.read()
     terms = data.split("\n")
@@ -11,6 +15,8 @@ def load_user_password():
 
 def set_proxy():
     terms = load_user_password()
+    if terms is False:
+        return
     import os
 
     proxy = f'http://{terms[0]}:{terms[1]}@10.4.103.143:8080'

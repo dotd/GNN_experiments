7612281308640a1d54269ece2cd02f1f06349515
joeloren <z\E/5cgw>
2021-01-13 11:46:08 +0200

    add random pruning

--- tst/ogb/main_pyg_with_lsh_pruning.py
+++ tst/ogb/main_pyg_with_lsh_pruning.py
@@ -8,7 +8,7 @@ from ogb.graphproppred import PygGraphPropPredDataset, Evaluator
 from torch_geometric.data import DataLoader
 from tqdm import tqdm
 
-from src.utils.graph_prune_utils import tg_dataset_prune_edges_by_minhash_lsh
+from src.utils.graph_prune_utils import tg_dataset_prune
 from src.utils.lsh_euclidean_tools import LSH
 from src.utils.minhash_tools import MinHash
 from src.utils.proxy_utils import set_proxy
@@ -88,6 +88,8 @@ def main():
                         help='filename to output result (default: )')
     parser.add_argument('--proxy', action="store_true", default=False,
                         help="Set proxy env. variables. Need in bosch networks.", )
+    parser.add_argument('--pruning_method', type=str, default='random')
+    parser.add_argument('--random_pruning_prob', type=float, default=.5)
     args = parser.parse_args()
 
     device = torch.device("cuda:" + str(args.device)) if torch.cuda.is_available() else torch.device("cpu")
@@ -96,22 +98,30 @@ def main():
         set_proxy()
 
     rnd = np.random.RandomState(0)
-    # MinHash parameters
-    num_minhash_funcs = 1
-    minhash = MinHash(num_minhash_funcs, rnd, prime=2147483647)
-    print(f"minhash:\n{minhash}")
 
-    # LSH parameters
-    lsh_num_funcs = 2
-    sparsity = 3
-    std_of_threshold = 1
-    dim_nodes = 9
-    lsh = LSH(dim_nodes,
-              num_functions=lsh_num_funcs,
-              sparsity=sparsity,
-              std_of_threshold=std_of_threshold,
-              random=rnd)
-    print(f"lsh:\n{lsh}")
+    if args.pruning_method == "minhash_lsh":
+        num_minhash_funcs = 1
+        minhash = MinHash(num_minhash_funcs, rnd, prime=2147483647)
+        # MinHash parameters
+        print(f"minhash:\n{minhash}")
+
+        # LSH parameters
+        lsh_num_funcs = 2
+        sparsity = 3
+        std_of_threshold = 1
+        dim_nodes = 9
+        lsh = LSH(dim_nodes,
+                  num_functions=lsh_num_funcs,
+                  sparsity=sparsity,
+                  std_of_threshold=std_of_threshold,
+                  random=rnd)
+        print(f"lsh:\n{lsh}")
+
+        prune_args = {"minhash": minhash, "lsh": lsh}
+    elif args.pruning_method == "random":
+        prune_args = {"random": rnd, "p": args.random_pruning_prob}
+    else:
+        raise ValueError("Invalid pruning method")
 
     # automatic data loading and splitting
     dataset = PygGraphPropPredDataset(name=args.dataset)
@@ -122,12 +132,12 @@ def main():
 
     # orig_train_data_num_edges = train_data.data.edge_index.shape[1]
     old_avg_edge_count = np.mean([g.edge_index.shape[1] for g in train_data])
-    tg_dataset_prune_edges_by_minhash_lsh(train_data, minhash, lsh)
+    tg_dataset_prune(train_data, args.pruning_method, **prune_args)
     avg_edge_count = np.mean([g.edge_index.shape[1] for g in train_data])
     print(
         f"Old average number of edges: {old_avg_edge_count}. New one: {avg_edge_count}. Change: {(old_avg_edge_count - avg_edge_count) / old_avg_edge_count * 100}\%")
-    tg_dataset_prune_edges_by_minhash_lsh(validation_data, minhash, lsh)
-    tg_dataset_prune_edges_by_minhash_lsh(test_data, minhash, lsh)
+    tg_dataset_prune(validation_data, args.pruning_method, **prune_args)
+    tg_dataset_prune(test_data, args.pruning_method, **prune_args)
 
     if args.feature == 'full':
         pass

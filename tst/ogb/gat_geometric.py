0e541d3e2343283e61ade24987a0d37be95bc2b4
eitank <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>
2021-06-08 09:17:04 -0400

    ADD gat model

--- tst/ogb/gat_geometric.py
+++ tst/ogb/gat_geometric.py
@@ -8,20 +8,15 @@ class GAT(torch.nn.Module):
     def __init__(self, num_features, num_classes):
         super(GAT, self).__init__()
         hidden_channels = 16
-        self.conv1 = GATConv(num_features, 32)
-        self.conv2 = GATConv(32, 64)
-        self.conv3 = GATConv(64, 128)
-        self.conv4 = GATConv(128, 256)
+        self.conv1 = GATConv(num_features, 8, heads=8)
+        self.conv2 = GATConv(8, num_classes, heads=1)
+        # self.conv3 = GATConv(64, 128)
+        # self.conv4 = GATConv(128, 256)
         # self.conv3 = GCNConv(hidden_channels, hidden_channels)
         # self.conv1 = SplineConv(1, 32, dim=1, kernel_size=5)
 
-        self.lin1 = nn.Linear(256, 128)
-        self.lin2 = nn.Linear(128, 1)
-
-        # self.conv1 = GATConv(num_features, 8, heads=8, dropout=0.6)
-        # On the Pubmed dataset, use heads=8 in conv2.
-        # self.conv2 = GATConv(8 * 8, num_classes, heads=1, concat=False,
-        #                      dropout=0.6)
+        # self.lin1 = nn.Linear(256, 128)
+        # self.lin2 = nn.Linear(128, 1)
 
     def forward(self, data):
         x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
@@ -44,3 +39,20 @@ class GAT(torch.nn.Module):
         x = F.elu(x)
         x = self.lin2(x)
         return x
+
+
+class GAT_Reddit(torch.nn.Module):
+    def __init__(self, num_features, num_classes):
+        super(GAT_Reddit, self).__init__()
+        self.num_layers = 2
+        self.conv1 = GATConv(num_features, 8, heads=8)
+        self.conv2 = GATConv(64, num_classes, heads=1)
+        self.convs = torch.nn.ModuleList([self.conv1, self.conv2])
+
+    def forward(self, x, adjs):
+        for i, (edge_index, _, size) in enumerate(adjs):
+            x_target = x[:size[1]]  # Target nodes are always placed first.
+            x = self.convs[i]((x, x_target), edge_index)
+            if i != self.num_layers - 1:
+                x = F.elu(x)
+        return x.log_softmax(dim=-1)

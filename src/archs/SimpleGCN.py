134bc3508eb5ccf2ceb183a8d485db6ccb633e79
eitank <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>
2021-06-15 07:45:48 -0400

    REFORMAT SimpleGCN

--- src/archs/SimpleGCN.py
+++ src/archs/SimpleGCN.py
@@ -1,21 +1,22 @@
-import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, global_mean_pool, SplineConv, GATConv
 import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, global_mean_pool
+
+
+# Created by: Eitan Kosman, BCAI
+# A simple GCN architecture, generally used for testing
 
 
 class SimpleGCN(nn.Module):
-    def __init__(self, num_node_features, num_classes):
+    def __init__(self, num_node_features, num_classes=10):
         super(SimpleGCN, self).__init__()
-        hidden_channels = 16
-        self.conv1 = GATConv(num_node_features, 32)
-        self.conv2 = GATConv(32, 64)
-        self.conv3 = GATConv(64, 128)
-        self.conv4 = GATConv(128, 256)
-        # self.conv3 = GCNConv(hidden_channels, hidden_channels)
-        # self.conv1 = SplineConv(1, 32, dim=1, kernel_size=5)
+        self.conv1 = GCNConv(num_node_features, 32)
+        self.conv2 = GCNConv(32, 64)
+        self.conv3 = GCNConv(64, 128)
+        self.conv4 = GCNConv(128, 256)
 
         self.lin1 = nn.Linear(256, 128)
-        self.lin2 = nn.Linear(128, 10)
+        self.lin2 = nn.Linear(128, num_classes)
 
     def forward(self, data):
         x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
@@ -37,6 +38,3 @@ class SimpleGCN(nn.Module):
         x = F.elu(x)
         x = self.lin2(x)
         return x
-        # return F.log_softmax(x, dim=1)
-
-

e25d1b24bd37186920490ae70ea9b56f93c233e3
eitank <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>
2021-07-19 10:28:23 +0300

    FIX gat

--- src/archs/node_prediction.py
+++ src/archs/node_prediction.py
@@ -7,15 +7,16 @@ class NodeGat(nn.Module):
     def __init__(self, num_features, num_classes, num_hidden, num_heads, n_hidden_layers=2):
         super(NodeGat, self).__init__()
         self.conv1 = GATConv(num_features, num_hidden, num_heads)
-        self.conv2 = nn.Sequential(*[GATConv(num_hidden * num_heads, num_hidden, num_heads) for _ in range(n_hidden_layers)])
+        self.conv2 = nn.ModuleList([GATConv(num_hidden * num_heads, num_hidden, num_heads) for _ in range(n_hidden_layers - 1)])
         self.clf = nn.Linear(num_hidden * num_heads, num_classes)
 
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
-        x = F.relu(x)
+        for l in self.conv2:
+            x = l(x, edge_index)
+            x = F.relu(x)
         x = self.clf(x)
         return F.log_softmax(x, dim=1)
 
